6c8d00cef817d8ba352367f22041c877868a74f5
unknown <eisa@email.uscb.edu>
2024-06-09 11:58:41 +0530

    env var and server setup done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Eisa is running")
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+// const connectDb = require("./config/dbConnection");
+const dotenv = require("dotenv").config(); // imports the module where 
+                                           // it loads env variables from a `.env` file into process.env
+                                           // the env vars are accessible to all processes
+
+const app = express();                                  
+
+app.use(express.json()); // Useful for parsing data from HTTP `requests`
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+// All of the above initialization is necessary before the server starts to listen
+// so that the code executes and responds how we will it to
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
+
---
+++ middleware/errorHandler.js
@@ -0,0 +1,53 @@
+const { constants } = require("./constants");
+
+const errorHandler = (err, req, res, next) => { // So a response always returns with these four parameters
+    const statusCode = res.statusCode ? res.statusCode : 500; // There is always a statusCode attached with the response
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({
+                title: "Validation Failed",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.NOT_FOUND:
+            res.json({
+                title: "Not Found",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        case constants.SERVER_ERROR:
+            res.json({
+                title: "Server Error",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+        default:
+            res.json({
+                title: "Some kind of error",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            console.log("No error, all good!");
+            break;
+    }
+
+};
+
+module.exports = errorHandler;
