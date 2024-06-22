4d6dd3380d54c31be4d1098771e52351a9925dc5
unknown <eisa@email.uscb.edu>
2024-06-22 15:06:19 +0530

    Changed the file name and made adjustments for deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv").config(); // imports the module where 
+                                           // it loads env variables from a `.env` file into process.env
+                                           // the env vars are accessible to all processes
+
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const cors = require('cors');
-const dotenv = require("dotenv").config(); // imports the module where 
-                                           // it loads env variables from a `.env` file into process.env
-                                           // the env vars are accessible to all processes
+
 connectDb();
 const app = express(); 
                                  
@@ -18,11 +20,11 @@ app.use("/api/events", require("./routes/eventRoutes")); // Any requests to "/ap
                                                        // will be handled by the routes defined in the mentioned file
 app.use(errorHandler); // Have switch statements with status codes in it
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // All of the above initialization is necessary before the server starts to listen
 // so that the code executes and responds how we will it to
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`server is running on port ${port}`);
 });
 
