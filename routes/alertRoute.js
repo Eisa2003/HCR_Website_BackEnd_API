b86b38c8bbe2853e4a92b96e2b498c381881fe97
unknown <eisa@email.uscb.edu>
2024-08-13 15:13:44 +0530

    Fixed the alert router

--- routes/alertRoute.js
+++ routes/alertRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { updateAlert, getAlert } = require("../controllers/alertController");
 const validateToken = require("../middleware/validateTokenHandler");
 
-router.put("/", validateToken, updateAlert) // first middleware to be executed is itself,
+router.route('/:id').put(validateToken, updateAlert) // first middleware to be executed is itself,
                                                    // then validateToken and at last updateAlert
 
 router.route('/').get(getAlert)
